f3ef7be4a1dd4ae57480a2a19eca2a5c5b092a77
Veny Cordova <venycordova@gmail.com>
2022-09-25 21:07:49 -0400

    added the function

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url, left, bottom) {
+    let greenCharacter = document.createElement("img");    
+    greenCharacter.src = "assets/green-character.gif";
+    greenCharacter.style.position = "fixed";
+    greenCharacter.style.left = "100px";
+    greenCharacter.style.bottom = "100px";    
+    document.body.append(greenCharacter);
+    return greenCharacter
+}  
+newImage('assets/green-character.gif', 100, 100)
 
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
+
+
+
+    
+
+
+    let pineTree = document.createElement("img");
+    pineTree.src = "assets/pine-tree.png";
+    pineTree.style.position = "fixed";
+    pineTree.style.left = "450px";
+    pineTree.style.bottom = "200px";
+    document.body.append(pineTree);
+
