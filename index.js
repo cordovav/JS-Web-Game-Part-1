5c7574af671de9760b22a305e7582ba5903d2a64
Veny Cordova <venycordova@gmail.com>
2022-09-25 22:05:57 -0400

    added images

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 function newImage(url, left, bottom) {
-    let newCharacter = document.createElement("img");    
-    newCharacter.src = url;
-    newCharacter.style.position = "fixed";
-    newCharacter.style.left = "100px";
-    newCharacter.style.bottom = "100px";    
-    document.body.append(newCharacter);
-    return newCharacter
+    let img = document.createElement("img");    
+    img.src = url;
+    img.style.position = 'fixed';
+    img.style.left = left  ;
+    img.style.bottom = bottom  ;    
+    document.body.append(img);
+    return img
 }  
 
-newImage("assets/green-character.gif", 100, 100)
-newImage("assets/pine-tree.png", 450, 200)
+newImage("assets/green-character.gif", "100px", "100px")
+newImage("assets/pine-tree.png", "450px", "200px")
+newImage("assets/tree.png", '200px', '300px')
+newImage("assets/pillar.png", '350px', '100px')
+newImage("assets/crate.png", '150px', '200px')
+newImage("assets/well.png", '500px', '420px')
 
 
 
@@ -18,4 +22,3 @@ newImage("assets/pine-tree.png", 450, 200)
 
 
     
-
