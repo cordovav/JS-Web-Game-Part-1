c02aff133e82e734f0a43d76c80d8a5182455af0
Veny Cordova <venycordova@gmail.com>
2022-09-25 21:16:27 -0400

    added the pine tree into function

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 function newImage(url, left, bottom) {
-    let greenCharacter = document.createElement("img");    
-    greenCharacter.src = "assets/green-character.gif";
-    greenCharacter.style.position = "fixed";
-    greenCharacter.style.left = "100px";
-    greenCharacter.style.bottom = "100px";    
-    document.body.append(greenCharacter);
-    return greenCharacter
+    let newCharacter = document.createElement("img");    
+    newCharacter.src = url;
+    newCharacter.style.position = "fixed";
+    newCharacter.style.left = "100px";
+    newCharacter.style.bottom = "100px";    
+    document.body.append(newCharacter);
+    return newCharacter
 }  
-newImage('assets/green-character.gif', 100, 100)
+
+newImage("assets/green-character.gif", 100, 100)
+newImage("assets/pine-tree.png", 450, 200)
 
 
 
@@ -15,10 +17,5 @@ newImage('assets/green-character.gif', 100, 100)
     
 
 
-    let pineTree = document.createElement("img");
-    pineTree.src = "assets/pine-tree.png";
-    pineTree.style.position = "fixed";
-    pineTree.style.left = "450px";
-    pineTree.style.bottom = "200px";
-    document.body.append(pineTree);
+    
 
